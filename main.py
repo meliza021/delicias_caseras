d03d4da1544252b2f6d50177d60820009ebea3f1
meliza021 <189984449+meliza021@users.noreply.github.com>
2025-01-11 16:34:10 -0500

    actividad#4

--- main.py
+++ main.py
@@ -1,26 +1,17 @@
-from design.products import design, tableProducts
-from design.products import design, tableProducts, tableProductsByCategory
-from logic.products import updateInventoryByCode, newproducts
+from design.products import desing, tableProducts, tableProductsByCategory
+from logic.products import updateInventoryByCode
+from logic.products import updateInventoryByCode, newProduct
 
-match design():
+match desing():
+    case 0:
+        print("¡Hasta luego!")
     case 1:
         tableProducts()
     case 2:
-        tableProductsByCategory(input("Ingrese la categoria. Ejemplo ('panes', 'pastel', 'postres'): "
-        ).lower())
+        tableProductsByCategory(input("Ingrese la categoria a buscar, ejemplo (Panes, Pastel, Postres): "))
     case 3:
-        updateInventoryByCode(input("Ingrese el codigo del producto: ") )
-    case 4:
-        codigo = input("ingrese el codigo_producto ejemplo (PN-001): ")
-        nombre = input("ingrese el nombre del producto ejemplo (galletas): ")
-        cat = input("ingrese la categoria del producto:  ")
-        desc = input("ingrese la descripcio del producto: ")
-        prov = input("ingrese el proveedor del producto (Panadería Natural):  ")
-        cant_sto = int(input("ingrese la cantidad de stock:  "))
-        precio = float(input("ingrese el precio de venta: "))
-        precioPro = float(input("ingrese el precio de proveedor: ") )
-        newproducts(codigo, nombre, cat, desc, prov, cant_sto, precio, precioPro)
-    case 0:
-        print("Chao")
+        updateInventoryByCode(input("Ingrese el codigo de producto, ejemplo (PN-001): "))
+    case 4: 
+        newProduct()
     case _:
-        print("Esa opción no existe")
+        print("Esa opcion no existe")
