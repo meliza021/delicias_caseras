c9ef73a0d070a49cda6cc31f824095c5061c861d
meliza021 <189984449+meliza021@users.noreply.github.com>
2025-01-14 15:59:10 -0500

    cambios relizados ademas agregamos la opcion de editar pedidos

--- main.py
+++ main.py
@@ -1,18 +1,3 @@
-from design.products import desing , tableProducts, addProduct, tableProductsBycategory
-from logic.products import updateInventoryBycode
 
-def main():
-    match desing():  
-        case 1:
-            addProduct()  
-        case 2:
-            tableProducts()
-        case 3:
-            tableProductsBycategory(input("Ingrese la Categoria ejemplo (panes,pasteles,postres): "))
-        case 0:
-            print("Gracias por utilizar el sistema")
-        case _: 
-            print("Esta opción no existe")
-
-
-main()
+from design.general import *
+menu()
